7c3e608daba69098a218d094b0902ca76c100551
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-11 20:06:20 -0300

    levantando servidor

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+
+app.use (express.static (publicPath));
+
+app.get ('/',(req,res) => {
+    res.sendFile (path.join (__dirname, './views/carrito.html'))
+});
+
+app.listen (3000,() => console.log("servidor levantado")
+);
