bbb89fc7de1fc3115f766484449c013425096a3a
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-12 17:30:30 -0300

    cree una variable port

--- site_mundoPets/app.js
+++ site_mundoPets/app.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const path = require ('path');
 
 const app = express();
+const port = 3000
+
 const publicPath = path.resolve(__dirname, './public');
 
 app.use (express.static (publicPath));
@@ -10,5 +12,5 @@ app.get ('/',(req,res) => {
     res.sendFile (path.join (__dirname, './views/carrito.html'))
 });
 
-app.listen (3000,() => console.log("servidor levantado")
+app.listen (port,() => console.log("servidor levantado")
 );
