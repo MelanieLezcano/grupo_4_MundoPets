7fcbd390a08296b2c815adaa271d60c8702f645d
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-13 21:55:38 -0300

    agrego img y arreglo ruta carrito

--- site_mundoPets/app.js
+++ site_mundoPets/app.js
@@ -8,7 +8,7 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use (express.static (publicPath));
 
-app.get ('/',(req,res) => {
+app.get ('/carrito',(req,res) => {
     res.sendFile (path.join (__dirname, './views/carrito.html'))
 });
 
