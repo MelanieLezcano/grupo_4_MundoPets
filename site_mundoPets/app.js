42c571900c2e367004be0763e906d1401d2af3a2
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-16 23:50:46 -0300

    agrego rutas

--- site_mundoPets/app.js
+++ site_mundoPets/app.js
@@ -8,8 +8,11 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use (express.static (publicPath));
 
+app.get ('/',(req,res) => {res.sendFile (path.join (__dirname, './views/home.html'))});
 app.get ('/carrito',(req,res) => {res.sendFile (path.join (__dirname, './views/carrito.html'))});
 app.get ('/detalle',(req,res) => {res.sendFile (path.join (__dirname, './views/detalle.html'))});
+app.get ('/register',(req,res) => {res.sendFile (path.join (__dirname, './views/register.html'))});
+app.get ('/login',(req,res) => {res.sendFile (path.join (__dirname, './views/login.html'))});
 
 app.listen (port,() => console.log("servidor levantado")
 );
