e49f011b4d045ee3ba6e2e7bb471ee6798136d61
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-16 21:47:54 -0300

    agrego div

--- site_mundoPets/app.js
+++ site_mundoPets/app.js
@@ -8,9 +8,8 @@ const publicPath = path.resolve(__dirname, './public');
 
 app.use (express.static (publicPath));
 
-app.get ('/carrito',(req,res) => {
-    res.sendFile (path.join (__dirname, './views/carrito.html'))
-});
+app.get ('/carrito',(req,res) => {res.sendFile (path.join (__dirname, './views/carrito.html'))});
+app.get ('/detalle',(req,res) => {res.sendFile (path.join (__dirname, './views/detalle.html'))});
 
 app.listen (port,() => console.log("servidor levantado")
 );
