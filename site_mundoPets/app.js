12eb9307e3ec32353d1868440e13b4e30d06521a
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-07-18 21:21:16 -0300

    agregando fondo

--- site_mundoPets/app.js
+++ site_mundoPets/app.js
@@ -13,6 +13,7 @@ app.get ('/carrito',(req,res) => {res.sendFile (path.join (__dirname, './views/c
 app.get ('/detalle',(req,res) => {res.sendFile (path.join (__dirname, './views/detalle.html'))});
 app.get ('/register',(req,res) => {res.sendFile (path.join (__dirname, './views/register.html'))});
 app.get ('/login',(req,res) => {res.sendFile (path.join (__dirname, './views/login.html'))});
+app.get ('*',(req,res) => {res.sendFile (path.join (__dirname, './views/404.html'))});
 
 app.listen (port,() => console.log("servidor levantado")
 );
