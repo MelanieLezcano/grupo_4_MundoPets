a7077b47c9e9ad88dfe77217d766c2052624115d
MercedesAAlvarez <mechychevy@gmail.com>
2022-09-18 18:53:00 -0300

    cambios header

--- site_mundoPets/src/app.js
+++ site_mundoPets/src/app.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-const port = 3018;
+const port = 3000;
 const path = require ('path');
 const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
