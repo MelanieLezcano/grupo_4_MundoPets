72d5bd694cc33d552f6cb3bc7be30e9b67a9ee40
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-10-29 01:01:33 -0300

    sequelize db listos

--- site_mundoPets/src/database/seeders/20221018172224-categorias.js
+++ site_mundoPets/src/database/seeders/20221018172224-categorias.js
@@ -1,42 +1,27 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     let categoria = ['Perro','Gato']
     
       let x = [ {
         nombre:'Perro',
         banner:'no se',
-      /*   createdAt:new Date,
-        updatedAt: new Date */
+        createdAt:new Date,
+        updatedAt: new Date
       },
       {
         nombre:'Gato',
         banner:'no se',
-       /*  createdAt:new Date,
-        updatedAt: new Date */
+        createdAt:new Date,
+        updatedAt: new Date
       }
     ]
-   
+
+    module.exports = {
+    async up (queryInterface, Sequelize) {
      await queryInterface.bulkInsert('Categorias',x, {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
      await queryInterface.bulkDelete('Categorias', null, {});
-  }
+  },
 };
