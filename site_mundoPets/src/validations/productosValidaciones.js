a60219c97a5aeb360eb4a49b84aba144bc7da899
MelanieLezcano <melanie.s.lezcano@gmail.com>
2022-11-27 21:42:30 -0300

    validaciones de back

--- site_mundoPets/src/validations/productosValidaciones.js
+++ site_mundoPets/src/validations/productosValidaciones.js
@@ -4,7 +4,7 @@ module.exports = [
     /* TITULO */
     check('Titulo').trim()
     .notEmpty().withMessage('Este campo es obligatorio').bail()
-    .isLength({min:5}).withMessage('Debe contener al menos 5 caracteres'),
+    .isLength({min: 2, max: 70 }).withMessage('Debe contener al menos 2 caracteres'),
 
     /* MARCA */
     check('Marca').trim()
@@ -13,16 +13,19 @@ module.exports = [
     /* PRECIO */
     check('Precio').trim()
     .notEmpty().withMessage('Este campo es obligatorio').bail()
-    .isInt().withMessage('Solo se aceptan números'),
+    .isInt().withMessage('Solo se aceptan números').bail()
+    .isLength({min: 2, max: 16 }).withMessage('Debe contener al menos 2 caracteres'),
 
     /* DESCUENTO */
     check('Descuento').trim()
-    .isInt().withMessage('Sólo se aceptan números'),
+    .isInt().withMessage('Sólo se aceptan números').bail()
+    .isLength({min: 2, max: 2 }).withMessage('Debe contener al menos 2 caracteres'),
 
     /* STOCK */
     check('Stock').trim()
     .notEmpty().withMessage('Este campo es obligatorio').bail()
-    .isInt().withMessage('Sólo se aceptan números'),
+    .isInt().withMessage('Sólo se aceptan números').bail()
+    .isLength({min: 1, max: 3 }).withMessage('Debe contener al menos 3 caracteres'),
    
     /* CATEGORIA */
     check('Categoria').trim()
@@ -35,6 +38,6 @@ module.exports = [
     /* DESCRIPCION */
     check('Descripcion').trim()
     .notEmpty().withMessage('Este campo es obligatorio').bail()
-    .isLength({min:10}).withMessage('Debe contener al menos 10 caracteres')
+    .isLength({min:10, max: 255 }).withMessage('Debe contener al menos 10 caracteres')
 ]
 
