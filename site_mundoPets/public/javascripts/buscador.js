280f86d11f7e28dd295bcfb86ab14a7b75ab5459
MercedesAAlvarez <mechychevy@gmail.com>
2022-12-16 23:13:42 -0300

    subiendo alguns cambios en header

---
+++ site_mundoPets/public/javascripts/buscador.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+    let vinculacion = 'Buscador vinculado con exito'
+    console.log(vinculacion);
+
+    let $ = (elemento) => document.querySelector(elemento)
+
+    let barraBusqueda = $('#barraBusqueda')
+    let buscador = $('#search')
+    let palabra = ""
+
+    buscador.onkeydown = (evento) => {
+        let letra = evento.key 
+        letra === 'Backspace' ? palabra = palabra.substring(0, palabra.length - 1) : letra.length > 1 ? null :
+        palabra = palabra += letra
+    }
+    
+
+
+    barraBusqueda.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+
+        console.log(barraBusqueda.elementos);
+       /*  if () {
+            barraBusqueda.submit()
+        }   */
+    })
+
+
+    
+
+       
+
+
+
+
+})
